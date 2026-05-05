94fbaadbea5d6087ad345539883bd32773f314f1
Pankaj Bam <bam.pankaj2820@gmail.com>
2026-05-05 22:25:01 +0300

    add e2e tests with playwright

---
+++ playwright.config.js
@@ -0,0 +1,20 @@
+const { defineConfig, devices } = require('@playwright/test')
+
+module.exports = defineConfig({
+  testDir: './e2e-tests',
+  timeout: 30000,
+  use: {
+    baseURL: 'http://localhost:8080',
+  },
+  webServer: {
+    command: 'npm start',
+    url: 'http://localhost:8080',
+    reuseExistingServer: !process.env.CI,
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+})
